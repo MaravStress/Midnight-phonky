db308d80e63cd0abc30c56977737fd2900c56089
Eliam Paredes <67697491+MaravStress@users.noreply.github.com>
2022-12-16 11:11:48 -0400

    e

--- Midnight phonky/Assets/Scenes/IA_ControlMoto.cs
+++ Midnight phonky/Assets/Scenes/IA_ControlMoto.cs
@@ -18,9 +18,9 @@ public class IA_ControlMoto : MonoBehaviour
         if (Vector3.Distance(transform.position, nav.transform.position) > rangoMax)
             nav.transform.position = transform.position;
         
-        //move = new Vector3(Mathf.Atan2(nav.transform.forward.x, nav.transform.forward.z) * Mathf.Rad2Deg,0,1 * rotacion).normalized/rotacion;
+        move = new Vector3(Mathf.Atan2(nav.transform.forward.x, nav.transform.forward.z) * Mathf.Rad2Deg,0,1 * rotacion).normalized/rotacion;
         //move.y = 0;
        // transform.rotation = nav.transform.rotation;
-        moto.Muevete(new Vector2(0, 0.4f));
+        moto.Muevete(new Vector2(move.x, 0.4f));
     }
 }
