96d583d548f28e861fb75360246e957a6935ecf7
MaravStress <eliamjesus@gmail.com>
2022-12-17 00:10:03 -0400

    La ia funciona

--- Midnight phonky/Assets/Scenes/IA_ControlMoto.cs
+++ Midnight phonky/Assets/Scenes/IA_ControlMoto.cs
@@ -8,8 +8,8 @@ public class IA_ControlMoto : MonoBehaviour
     public Moto moto;
     public NavMeshAgent nav;
 
-    public float rangoMax = 3,rotacion = 4f;
-    public Vector3 move;
+    public float rangoMax = 3,speed = 1, giro = 2;
+    public Vector2 move;
     public void Destino(Vector3 punto) {
         nav.SetDestination(punto);
     }
@@ -18,9 +18,8 @@ public class IA_ControlMoto : MonoBehaviour
         if (Vector3.Distance(transform.position, nav.transform.position) > rangoMax)
             nav.transform.position = transform.position;
         
-        move = new Vector3(Mathf.Atan2(nav.transform.forward.x, nav.transform.forward.z) * Mathf.Rad2Deg,0,1 * rotacion).normalized/rotacion;
-        //move.y = 0;
-       // transform.rotation = nav.transform.rotation;
-        moto.Muevete(new Vector2(move.x, 0.4f));
+        move.x = Vector3.Cross(transform.forward, nav.transform.forward).y * giro;
+        move.y = ((180 - Vector3.Angle(transform.forward, nav.transform.forward))/360)*speed;
+        moto.Muevete(move);
     }
 }
